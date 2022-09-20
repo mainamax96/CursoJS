486e6efc811e110d385686ed64710f86d7823226
mainamax96 <67932116+mainamax96@users.noreply.github.com>
2022-09-20 16:49:51 -0500

    Create Ejercicio 5.js

---
+++ Sesion 5/Ejercicio 5.js
@@ -0,0 +1,12 @@
+let alturaCm = 180
+let alturaM = 1.80
+let pesoKl = 71.55
+
+let alturaRoundM = Math.ceil(alturaM)
+console.log(alturaRoundM);
+
+let pesoRoundKl = Math.floor(pesoKl)
+console.log(pesoRoundKl);
+
+let maxValJsCompare = (Number.MAX_VALUE == Number.MAX_VALUE + 1)
+console.log(maxValJsCompare);
