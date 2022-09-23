a1eb3b2a3f5046d87515eb5d33e874bf134f9e99
mainamax96 <67932116+mainamax96@users.noreply.github.com>
2022-09-23 00:10:23 -0500

    Create Ejercicio 7-1.js

---
+++ Sesion 07 - Tipos de objetos/Ejercicio 7-1.js
@@ -0,0 +1,11 @@
+// - Un nuevo Set con los nombres de tu familia
+const arrayFam = ["Alexander","Venus","Lili","Fatima"]
+const familia = new Set(arrayFam)
+console.log(familia)
+// - Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+familia.add("Alexander")
+console.log(familia);
+
+// - Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
+familia.add("Javascript")
+console.log(familia);
